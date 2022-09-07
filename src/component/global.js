d8ac804279b1f26ec85acb599c6abfa155817dd8
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2022-09-07 15:05:41 +0600

    work done 4

--- src/component/global.js
+++ src/component/global.js
@@ -6,6 +6,8 @@ import {
 import { useState, useEffect } from "react";
 import { BiPlay, BiPause, BiVolumeLow } from "react-icons/bi";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import instructionMsgImg from "../assets/Capture.PNG";
+import playIcon from "../assets/playIcon.png";
 
 const TIMETOSHOW = 3;
 function App() {
@@ -19,7 +21,7 @@ function App() {
   const [playable, setPlayable] = useState(false);
   const [playable2, setPlayable2] = useState(false);
   const [isAutoPlay, setIsAutoPlay] = useState(false);
-  const [loader, setLoader] = useState(true);
+  const [instructionMsg, setInstructionMsg] = useState(true);
   //setting up the video to the state
   useEffect(() => {
     const video = document.getElementById("video_one");
@@ -28,7 +30,7 @@ function App() {
     setVideoNode(video);
     setVideoNode2(video2);
     // initializer();
-  }, []);
+  }, [instructionMsg, isAutoPlay]);
   // const initializer = async () => {
   //   setLoader(true);
 
@@ -61,6 +63,7 @@ function App() {
       });
       if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
         setIsAutoPlay(true);
+        setPlayable(true);
         setPlayable2(true);
       }
 
@@ -97,20 +100,17 @@ function App() {
         videoNode.pause();
       });
       videoNode?.addEventListener("canplay", (...args) => {
-        // setPlayable(true);
-        console.log("hello");
         if (playable && playable2) {
           videoNode2?.play();
         }
       });
       videoNode2?.addEventListener("canplay", (...args) => {
-        // setPlayable2(true);
         if (playable && playable2) {
           videoNode?.play();
         }
       });
     }
-  }, [videoNode, videoNode2, playable, playable2]);
+  }, [videoNode, videoNode2, playable, playable2, instructionMsg]);
 
   useEffect(() => {
     const videos = document.querySelectorAll("video");
@@ -130,6 +130,7 @@ function App() {
 
   const playHandler = () => {
     const videos = document.querySelectorAll("video");
+
     Array.from(videos).forEach((video) => {
       if (video.paused) {
         video.play();
@@ -175,185 +176,234 @@ function App() {
       setPlaying(true);
     });
   };
+  useEffect(() => {
+    if (!instructionMsg) {
+      if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+        if (isAutoPlay) {
+          // alert("hello");
+        }
+      } else {
+        setTimeout(() => {
+          playHandler();
+        }, 1000);
+      }
+    }
+  }, [instructionMsg, isAutoPlay]);
   // if (loader) {
   //   return <div style={{ background: "white" }}>Loading......</div>;
   // }
+  const initialPlayer = () => {
+    setInstructionMsg((prestate) => !prestate);
+    if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+      setIsAutoPlay(true);
+      setPlayable(true);
+      setPlayable2(true);
+      setPlaying(true);
+    }
+    // playHandler();
+  };
+  console.log(playable, playable2, playing);
+
   return (
     <div className="App">
       <div className="container">
         <div className="row">
-          <div className="col-md-12 mt-4 mb-4">
-            <div className="video_wrapper mt-5">
-              <div
-                className="modal_one"
-                style={{
-                  zIndex: showModal ? 10000 : 0,
-                  opacity: showModal ? 1 : 0,
-                }}
-              >
-                <div className="close_btn">
-                  <button onClick={onCloseHandler}>
-                    Back To
-                    <br /> Video
-                  </button>
-                </div>
-
-                <div className="social_icons">
-                  <ul>
-                    <li>
-                      <a
-                        href={`https://www.facebook.com/sharer.php?u=${window.location.href}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <FaFacebookF />
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href={`https://twitter.com/intent/tweet?url=${window.location.href}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <FaTwitter />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <ReactCompareSlider
-                onlyHandleDraggable={true}
-                // handle={
-                //   // <ReactCompareSliderHandle
-                //   //   buttonStyle={{
-                //   //     backdropFilter: undefined,
-                //   //     background: "green",
-                //   //     border: 1,
-                //   //     height: "0",
-
-                //   //     color: "yellow",
-                //   //   }}
-                //   //   linesStyle={{
-                //   //     background: "green",
-                //   //   }}
-                //   // />
-                //   <div
-                //     style={{
-                //       display: "grid",
-                //       height: "100%",
-                //       placeContent: "center",
-                //     }}
-                //   >
-                //     <button
-                //       style={{
-                //         all: "unset",
-                //         borderRadius: "50%",
-                //         fontSize: 50,
-                //       }}
-                //     >
-                //       💥
-                //     </button>
-                //   </div>
-                // }
-                itemOne={
-                  <>
-                    <video
-                      playsInline
-                      autoPlay={isAutoPlay}
-                      muted
-                      width={"100%"}
-                      id="video_one"
-                      onTimeUpdate={timeUpdateHandler}
-                      onEnded={() => setPlaying(false)}
-                      preload="auto"
-                      onwaiting={() => {
-                        console.log(" video ,i am waiting");
-                      }}
-                      onStalled={(e) => console.log("hello", e)}
-                      // onCanPlayThrough={(e) => console.log("helllo", e)}
-                      // autoPlay={readyState1}
-                      // muted="muted"
-                    >
-                      <source
-                        src="https://bangabandhuzone.s3.ap-southeast-1.amazonaws.com/tamim_app_32.mp4"
-                        type="video/mp4"
-                      />
-                    </video>
-                  </>
-                }
-                itemTwo={
-                  <>
-                    <video
-                      autoPlay={isAutoPlay}
-                      muted
-                      playsInline
-                      width={"100%"}
-                      id="video_two"
-                      preload="auto"
-                      // autoPlay={readyState2}
-                      // muted="muted"
-                      onwaiting={() => {
-                        console.log(" video 2,i am waiting");
-                      }}
-                    >
-                      <source
-                        src="https://bangabandhuzone.s3.ap-southeast-1.amazonaws.com/tamim_app_12.mp4"
-                        type="video/mp4"
-                      />
-                    </video>{" "}
-                  </>
-                }
-              />
-
-              <div
-                className="img_btn"
-                style={{ opacity: showInfo === true ? 1 : 0 }}
-                onClick={openModal}
-              >
-                <img src="/image/tamim.png" alt="i am tamim" />
-              </div>
-            </div>
+          {instructionMsg ? (
             <>
-              {!showModal && (
-                <div className="control_panel">
-                  <div className="progress" onClick={progressHandler}>
-                    <div
-                      className="progress__filled"
-                      style={{ flexBasis: `${videoProgress}%` }}
-                    ></div>
+              <div className="col-md-12 mt-4 mb-4">
+                <div className="video_wrapper mt-5">
+                  <img
+                    style={{ width: "100%" }}
+                    src={instructionMsgImg}
+                    alt="instruction massage for Interactive video"
+                  />
+
+                  <div className="initial_Play_btn" style={{ opacity: 1 }}>
+                    <img
+                      src={playIcon}
+                      alt="i am tamim"
+                      onClick={initialPlayer}
+                    />
                   </div>
-                  <div className="play_icon">
-                    {playable && playable2 && playing ? (
-                      <BiPause
-                        onClick={playHandler}
-                        style={{ color: "white" }}
-                      />
-                    ) : (
-                      <>
-                        <BiPlay
-                          onClick={playHandler}
-                          style={{ color: "white" }}
-                        />
-                      </>
-                    )}
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      <BiVolumeLow style={{ color: "white" }} />
-                      <input
-                        type="range"
-                        className="slider"
-                        id="custom_range"
-                        min="0"
-                        max="1"
-                        step="0.05"
-                        value={volume}
-                        onChange={volumeHandler}
-                      ></input>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="col-md-12 mt-4 mb-4">
+                <div className="video_wrapper mt-5">
+                  <div
+                    className="modal_one"
+                    style={{
+                      zIndex: showModal ? 10000 : 0,
+                      opacity: showModal ? 1 : 0,
+                    }}
+                  >
+                    <div className="close_btn">
+                      <button onClick={onCloseHandler}>
+                        Back To
+                        <br /> Video
+                      </button>
+                    </div>
+
+                    <div className="social_icons">
+                      <ul>
+                        <li>
+                          <a
+                            href={`https://www.facebook.com/sharer.php?u=${window.location.href}`}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <FaFacebookF />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href={`https://twitter.com/intent/tweet?url=${window.location.href}`}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <FaTwitter />
+                          </a>
+                        </li>
+                      </ul>
                     </div>
                   </div>
+                  <ReactCompareSlider
+                    onlyHandleDraggable={true}
+                    // handle={
+                    //   // <ReactCompareSliderHandle
+                    //   //   buttonStyle={{
+                    //   //     backdropFilter: undefined,
+                    //   //     background: "green",
+                    //   //     border: 1,
+                    //   //     height: "0",
+
+                    //   //     color: "yellow",
+                    //   //   }}
+                    //   //   linesStyle={{
+                    //   //     background: "green",
+                    //   //   }}
+                    //   // />
+                    //   <div
+                    //     style={{
+                    //       display: "grid",
+                    //       height: "100%",
+                    //       placeContent: "center",
+                    //     }}
+                    //   >
+                    //     <button
+                    //       style={{
+                    //         all: "unset",
+                    //         borderRadius: "50%",
+                    //         fontSize: 50,
+                    //       }}
+                    //     >
+                    //       💥
+                    //     </button>
+                    //   </div>
+                    // }
+                    itemOne={
+                      <>
+                        <video
+                          playsInline
+                          autoPlay={isAutoPlay}
+                          muted
+                          width={"100%"}
+                          id="video_one"
+                          onTimeUpdate={timeUpdateHandler}
+                          onEnded={() => setPlaying(false)}
+                          preload="auto"
+                          onwaiting={() => {
+                            console.log(" video ,i am waiting");
+                          }}
+                          onStalled={(e) => console.log("hello", e)}
+                          // onCanPlayThrough={(e) => console.log("helllo", e)}
+                          // autoPlay={readyState1}
+                          // muted="muted"
+                        >
+                          <source
+                            src="https://bangabandhuzone.s3.ap-southeast-1.amazonaws.com/tamim_app_32.mp4"
+                            type="video/mp4"
+                          />
+                        </video>
+                      </>
+                    }
+                    itemTwo={
+                      <>
+                        <video
+                          autoPlay={isAutoPlay}
+                          muted
+                          playsInline
+                          width={"100%"}
+                          id="video_two"
+                          preload="auto"
+                          // autoPlay={readyState2}
+                          // muted="muted"
+                          onwaiting={() => {
+                            console.log(" video 2,i am waiting");
+                          }}
+                        >
+                          <source
+                            src="https://bangabandhuzone.s3.ap-southeast-1.amazonaws.com/tamim_app_12.mp4"
+                            type="video/mp4"
+                          />
+                        </video>{" "}
+                      </>
+                    }
+                  />
+
+                  <div
+                    className="img_btn"
+                    style={{ opacity: showInfo === true ? 1 : 0 }}
+                    onClick={openModal}
+                  >
+                    <img src="/image/tamim.png" alt="i am tamim" />
+                  </div>
                 </div>
-              )}
+                <>
+                  {!showModal && (
+                    <div className="control_panel">
+                      <div className="progress" onClick={progressHandler}>
+                        <div
+                          className="progress__filled"
+                          style={{ flexBasis: `${videoProgress}%` }}
+                        ></div>
+                      </div>
+                      <div className="play_icon">
+                        {playable && playable2 && playing ? (
+                          <BiPause
+                            onClick={playHandler}
+                            style={{ color: "white" }}
+                          />
+                        ) : (
+                          <>
+                            <BiPlay
+                              onClick={playHandler}
+                              style={{ color: "white" }}
+                            />
+                          </>
+                        )}
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          <BiVolumeLow style={{ color: "white" }} />
+                          <input
+                            type="range"
+                            className="slider"
+                            id="custom_range"
+                            min="0"
+                            max="1"
+                            step="0.05"
+                            value={volume}
+                            onChange={volumeHandler}
+                          ></input>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </>
+              </div>
             </>
-          </div>
+          )}
         </div>
       </div>
     </div>
