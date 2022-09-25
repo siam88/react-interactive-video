ec502a342a9e4d98d99ff5e13406ba561658c6e2
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2022-09-25 11:18:16 +0600

    dynamic data added

--- src/components/modals/modal2/index.js
+++ src/components/modals/modal2/index.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+
 import styles from "./index.module.css";
+import { LoginContext } from "../../../contexts/questionContext";
+
 const Modal2 = () => {
+  const { questions } = useContext(LoginContext);
+
   return (
     <div className={styles.quizBox}>
       <div className={styles.content}>
@@ -8,21 +13,21 @@ const Modal2 = () => {
           src={require(`../../../assets/images/marineDrive.png`)}
           alt="Modal1 background "
         />
-        <p>
-          কক্সবাজার-টেকনাফ মেরিন ড্রাইভ হল বঙ্গোপসাগর বরাবর কক্সবাজার থেকে
-          টেকনাফ পর্যন্ত 80 কিলোমিটার দীর্ঘ রাস্তা এবং এটি বিশ্বের দীর্ঘতম মেরিন
-          ড্রাইভ। এটি 6 মে 2017 তারিখে প্রধানমন্ত্রী শেখ হাসিনা উদ্বোধন করেন
-        </p>
+        <p>{questions[1]?.topic?.question[0]?.remarks}</p>
 
         <div className={styles.quiz}>
-          <div className={styles.question}>বাংলাদেশ কি বন্ধুত্বপূর্ণ দেশ?</div>
+          <div className={styles.question}>
+            {" "}
+            {questions[1]?.topic?.question[0]?.title}?
+          </div>
           <div className={styles.answers}>
-            <div className={`${styles.answer} ${styles.active}`}>
-              সম্পর্কের{" "}
-            </div>
-            <div className={styles.answer}>বন্ধুত্বপূর্ণ</div>
-            <div className={styles.answer}> আনুষ্ঠানিকভাবে</div>
-            <div className={styles.answer}>জাতির সাথে</div>
+            {/* <div className={`${styles.answer} ${styles.active}`}>
+              ব -দ্বীপের আবাসস্থল{" "}
+            </div> */}
+
+            {questions[1]?.topic?.question[0]?.options?.map((e, i) => (
+              <div className={styles.answer}>{e.option} </div>
+            ))}
           </div>
         </div>
       </div>
